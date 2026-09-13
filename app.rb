981fa2e9a320f2975124ad865355d3c514ca5988
arup <arup@dev.com>
2026-09-13 11:58:49 +0000

    Route bookings and reads through office A as the fixed authority

--- app.rb
+++ app.rb
@@ -1,15 +1,62 @@
 require "sinatra"
 require "json"
 require "thread"
+require "net/http"
 
 set :bind, "0.0.0.0"
 set :port, 4567
+set :host_authorization, {
+  permitted_hosts: [
+    "localhost",
+    "127.0.0.1",
+    "office-a",
+    "office-b"
+  ]
+}
 
 BOOKING = { customer: nil }
 BOOKING_LOCK = Mutex.new
+OFFICE = ENV.fetch("OFFICE", "a")
+AUTHORITY_URL = ENV.fetch("AUTHORITY_URL", "http://office-a:4567")
 
 before do
   content_type :json
+end
+
+helpers do
+  def forward_to_authority
+    uri = URI("#{AUTHORITY_URL}#{request.path_info}")
+
+    upstream_request =
+      if request.request_method == "POST"
+        Net::HTTP::Post.new(uri).tap do |req|
+          req["Content-Type"] = "application/json"
+          req.body = request.body.read
+        end
+      else
+        Net::HTTP::Get.new(uri)
+      end
+
+    response = Net::HTTP.start(
+      uri.host,
+      uri.port,
+      nil,
+      open_timeout: 1,
+      read_timeout: 2
+    ) do |http|
+      http.request(upstream_request)
+    end
+
+    halt response.code.to_i,
+         { "Content-Type" => "application/json" },
+         response.body
+  rescue Timeout::Error, SocketError, SystemCallError, EOFError
+    halt 503, {
+      error: "Cannot reach authority",
+      office: OFFICE,
+      detail: "A booking may have completed before communication failed."
+    }.to_json
+  end
 end
 
 get "/health" do
@@ -17,6 +64,8 @@ get "/health" do
 end
 
 get "/seat" do
+  forward_to_authority if OFFICE == "b"
+
   customer = BOOKING_LOCK.synchronize { BOOKING[:customer] }
 
   {
@@ -27,6 +76,8 @@ get "/seat" do
 end
 
 post "/book" do
+  forward_to_authority if OFFICE == "b"
+  
   begin
     payload = JSON.parse(request.body.read)
   rescue JSON::ParserError
